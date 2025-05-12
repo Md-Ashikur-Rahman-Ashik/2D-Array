da14ba9a51e4f7e118459c1547dd2304970d8f36
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-05-12 20:24:22 +0600

    Declaring two dimension array

---
+++ twoDimensionArray.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    // int integerArray[3][4];
+
+    // integerArray[1][2] = 10;
+
+    // printf("%d", integerArray[1][2]);
+
+    int row, column;
+    scanf("%d %d", &row, &column);
+
+    int integerArray[row][column];
+
+    return 0;
+}
