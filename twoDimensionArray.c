e7eb5a27cf440a8426a807dc2aa7b5511792857c
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-05-13 07:20:04 +0600

    Taken input in 2D array and printed the input

--- twoDimensionArray.c
+++ twoDimensionArray.c
@@ -13,5 +13,26 @@ int main()
 
     int integerArray[row][column];
 
+    for (int i = 0; i < row; i++)
+    {
+        /* code */
+        for (int j = 0; j < column; j++)
+        {
+            /* code */
+            scanf("%d", &integerArray[i][j]);
+        }
+    }
+
+    for (int i = 0; i < row; i++)
+    {
+        /* code */
+        for (int j = 0; j < column; j++)
+        {
+            /* code */
+            printf("%d ", integerArray[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
